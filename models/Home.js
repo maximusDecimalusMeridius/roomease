851c699fd50cf0a47174fb648d2c62323c561ee0
Clarice Tran <tran.clarice@gmail.com>
2023-02-09 12:04:42 -0800

    Created fields for Task and Home Sequelize models

--- models/Home.js
+++ models/Home.js
@@ -1,14 +1,31 @@
 //bring in Model and Datatypes from sequelize, define our connection
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
 
 class Home extends Model {}
 
 //Initialize the Home model
-Home.init({
-    //Add fields to model
-},{
-    sequelize
-});
+Home.init(
+	{
+		//Add fields to model
+		name: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			validate: {
+				isAlphanumeric: true,
+			},
+		},
+		zipcode: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			validate,
+		},
+	},
+	{
+		sequelize,
+		modelName: "home",
+		timestamps: false,
+	}
+);
 
-module.exports = Home
+module.exports = Home;
