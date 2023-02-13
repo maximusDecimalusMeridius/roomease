379aedeeaf51cb6fd54259c72931f821ef12ce57
Clarice Tran <tran.clarice@gmail.com>
2023-02-12 16:06:56 -0800

    delete functionality works on events page. Editing only works for what, date, and time data at the moment. still need to add editing functionality for attendees

--- public/js/events.js
+++ public/js/events.js
@@ -1,62 +1,121 @@
 let _eventModal = document.querySelector("#event-modal");
-let _addButton = document.querySelector("#add-event");
-let _modalCloseButton = document.querySelector(".close");
+let _addEventButton = document.querySelector("#add-event");
+const eventForm = document.getElementById("eventForm");
+const formWhat = document.getElementById("what");
+const formDate = document.getElementById("date");
+const formTime = document.getElementById("time");
+const formAttendees = document.getElementById("attendees");
 
-_addButton.addEventListener("click", (event) => {
+let edit = false;
+let eventId;
+let oldAttendeesList = [];
+
+// Add button for new tasks (display modals)
+_addEventButton.addEventListener("click", (event) => {
     event.preventDefault();
     _bedCover.style.display = "block";
     _eventModal.style.display = "block";
-})
-
-_modalCloseButton.addEventListener("click", () => {
-    _bedCover.style.display = "none";
-    _eventModal.style.display = "none";
+    edit = false;
+    eventForm.reset();
 });
 
-document.querySelector("#eventForm").addEventListener("submit", (e) => {
+eventForm.addEventListener("submit", (e) => {
     e.preventDefault();
-    const attendees = getSelectedAttendees(
-        document.getElementById("eventForm").elements["attendees"]
-    );
+    const attendees = getSelectedAttendees(eventForm.elements["attendees"]);
     const eventObj = {
-        what: document.querySelector("#what").value,
-        date: document.querySelector("#date").value,
-        time: document.querySelector("#time").value,
+        what: formWhat.value,
+        date: formDate.value,
+        time: formTime.value,
         attendees: attendees,
     };
-    console.log(eventObj);
     fetch("/events", {
         method: "POST",
         body: JSON.stringify(eventObj),
         headers: {
-            "Content-type": "application/json",
+            "Content-Type": "application/json",
         },
-    }).then((res) => {
-        console.log(res);
-        if (res.ok) {
-            alert("event added to database");
-        } else {
-            alert("could not process inputted data");
-        }
+    })
+        .then((res) => {
+            console.log(res.ok);
+            if (res.ok) {
+                location.href = "/events";
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
+
+document.querySelectorAll(".delete").forEach((deleteButton) => {
+    deleteButton.addEventListener("click", (e) => {
+        fetch(`/events/${e.target.parentNode.parentNode.parentNode.dataset.eventId}`, {
+            method: "DELETE",
+        })
+            .then((res) => {
+                if (res.ok) {
+                    location.href = "/events";
+                }
+            })
+            .catch((err) => {
+                console.log(err);
+            });
     });
 });
 
-document.querySelectorAll(".delete-button").forEach((deleteButton) => {
-    deleteButton.addEventListener("click", (e) => {
-        console.log(e.target.parentNode.parentNode.parentNode);
-        e.target.parentNode.parentNode.parentNode.remove();
+document.querySelectorAll(".edit").forEach((editButton) => {
+    editButton.addEventListener("click", (e) => {
+        e.preventDefault();
+        document.querySelectorAll(".edit-event-form").forEach((form) => {
+            form.classList.add("hide");
+        });
+        eventId = e.target.parentNode.parentNode.parentNode.dataset.eventId;
+        console.log(eventId);
+        edit = true;
+        const editEventForm = document.querySelector(`[data-edit-event-id="${eventId}"]`);
+        editEventForm.classList.remove("hide");
+    });
+});
+
+document.querySelectorAll(".edit-event-form").forEach((editEvent) => {
+    editEvent.addEventListener("submit", (e) => {
+        e.preventDefault();
+        const eventObj = {
+            what: document.querySelector(`[data-edit-what="${eventId}"]`).value,
+            date: document.querySelector(`[data-edit-date="${eventId}"]`).value,
+            time: document.querySelector(`[data-edit-time="${eventId}"]`).value,
+        };
+        fetch(`/events/${eventId}`, {
+            method: "PUT",
+            body: JSON.stringify(eventObj),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })
+            .then((res) => {
+                if (res.ok) {
+                    location.href = "/events";
+                }
+            })
+            .catch((err) => {
+                console.log(err);
+            });
     });
 });
 
 function getSelectedAttendees(list) {
     var selected = [];
-    for (var i = 1; i < list.options.length; i++) {
+    for (var i = 0; i < list.options.length; i++) {
         if (list.options[i].selected == true) {
             selected.push(list.options[i].value);
         }
     }
     return selected;
 }
-function reloadThePage() {
-    window.location.reload(true);
-}
+
+document.getElementById("close").addEventListener("click", (e) => {
+    console.log("close button clicked");
+    _eventModal.style.display = "none";
+    _bedCover.style.display = "none";
+});
+
+function clearFormValues() {}
